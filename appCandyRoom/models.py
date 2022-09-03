8fcc0fcfa816697287f60c17d12317c9f3daeb19
YoanaGerling <yoanagerling@gmail.com>
2022-09-03 10:53:15 -0300

    Cree mis modelos, instale mi app

--- appCandyRoom/models.py
+++ appCandyRoom/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Stock(models.Model): 
+    producto = models.CharField(max_length = 50)
+    cantidad = models.IntegerField()
+    precio = models.FloatField()
+
+class Proveedores(models.Model):
+    nombre = models.CharField(max_length = 50)
+    apellido = models.CharField(max_length = 50)
+    email = models.EmailField()
+
+class Ventas(models.Model):
+    cliente = models.CharField(max_length = 50)
+    total = models.FloatField()
+    madio_de_pago = models.CharField(max_length = 50)
+
+class Gastos_Generales(models.Model):
+    especificacion = models.CharField(max_length = 50)
+    monto = models.FloatField() 
